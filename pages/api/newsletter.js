47e62aef4cfc1165ba30442671086aecab01b315
yseij <r0790156@student.thomasmore.be>
2022-02-07 15:09:07 +0100

    Foutafhandeling toevoegen

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,17 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://nestjs:1234@cluster0.o0r9z.mongodb.net/events?retryWrites=true&w=majority"
+  );
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const email = req.body.email;
@@ -9,12 +21,23 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://nestjs:1234@cluster0.o0r9z.mongodb.net/events?retryWrites=true&w=majority"
-    );
-    const db = client.db();
-    await db.collection("newsletter").insertOne({ email: email });
-    client.close();
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: "connectie met database mislukt" });
+      return;
+    }
+
+    try {
+      await insertDocument(client, { email: email });
+      client.close();
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "toevoegen van gegevens aan database mislukt" });
+      return;
+    }
 
     res.status(201).json({ message: "Signed Up" });
   }
